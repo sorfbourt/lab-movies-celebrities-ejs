04d21c88bc62cc2e37bc801b94895d2b1eb81add
Krystina Witt <krystinawitt@gmail.com>
2023-02-06 22:15:03 +0100

    completed iteration 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,18 +5,11 @@ const Celebrity = require('../models/Celebrity.model')
 const router = require('express').Router()
 // all your routes here
 
-router.get("/:id", async (req, res) => {
-    const movieID = req.params.id
-    const movieDetails = await Movie.findById(movieID).populate('cast')
-    console.log(movieDetails)
-    res.render("movies/movie-details", {movieDetails});
-});
-
 
 router.get("/create", async (req, res) => {
     try{
         const allCelebrities = await Celebrity.find()
-        //console.log(allCelebrities)
+        console.log(allCelebrities)
        
         res.render("movies/new-movie", {allCelebrities});
     }
@@ -27,9 +20,39 @@ router.get("/create", async (req, res) => {
 
 
 router.post("/create", async (req, res) => {
+    try{
     await Movie.create(req.body)
-    res.render("movies/movies");
+    const allMovies = await Movie.find().populate('cast')
+    res.render("movies/movies", {allMovies});
+}
+catch(error){
+    console.log(error)
+}
 });
+
+
+router.get("/:id", async (req, res) => {
+    try{
+        const movieID = req.params.id
+    const movieDetails = await Movie.findById(movieID).populate('cast')
+    console.log(movieDetails)
+    res.render("movies/movie-details", {movieDetails});
+    }
+    catch(error){
+        console.log(error)
+    }
+});
+
+
+
+
+router.post("/:id/delete", async (req, res) => {
+console.log("DELETE", req.params.id)
+await Movie.findByIdAndDelete(req.params.id)
+res.redirect("/")
+});
+
+
 
 
 router.get("/", async (req, res) => {
