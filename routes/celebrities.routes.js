70c83306c7e63097285fd5040c1beca7619a5a45
Krystina Witt <krystinawitt@gmail.com>
2023-02-06 19:02:01 +0100

    iteration 4 complete

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -6,29 +6,44 @@ const router = require('express').Router()
 router.get("/create", (req, res) => {
 
     res.render("celebrities/new-celebrity");
-  });
+});
 
 
-  router.get("/", (req, res) => {
-
-    res.render("celebrities");
-  });
+router.get('/', async (req, res) => {
 
 
+try {
+    const allCelebrities = await Celebrity.find()
+    console.log('all Celebrities :', allCelebrities)
+    //res.send('Hello!')
+    res.render('celebrities/celebrities', {allCelebrities})
+  } catch (error) {
+    console.log('Route to all celebrities', error)
+  }
+  })
+  
 
-  router.post("/create", async (req, res) => {
-      
-      try{
+
+router.post("/create", async (req, res) => {
+    
+    try{
         const newCelebrity = req.body
         console.log(newCelebrity)
         await Celebrity.create(req.body)
-        res.redirect("/");
+        res.redirect("/celebrities");
     }
     catch(error){
         console.log(error)
     }
     
-  });
+});
+
+
+// router.get("/celebrities", (req, res) => {
+//     res.render("celebrities");
+// });
+
+
 
 module.exports = router
 
